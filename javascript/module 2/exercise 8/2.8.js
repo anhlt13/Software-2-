b1be0fd613de81b3fedca94c938059a1bc597ac5
anhlt13 <anhlt@metropolia.fi>
2024-11-30 22:10:48 +0200

    Javascript module 2 exercise 8

---
+++ javascript/module 2/exercise 8/2.8.js
@@ -0,0 +1,10 @@
+function concat(array){
+  let result = "";
+  for( let i of array){
+    result+=i;
+  }
+  return target.textContent = `${result}`
+}
+const display = document.querySelector('#target');
+const array = ["happy", "birthday", "marry"];
+const print = concat(array);
